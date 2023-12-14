65d0e6ab89764052f5503eb7b8b68a23f9f1f861
marijmokoginta <marijmokoginta04@gmail.com>
2023-12-15 02:43:57 +0800

    add tatutan feature

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,88 +1,4 @@
 <footer class="bg-whitemax-w-screen-xl p-4 py-6 mx-auto lg:py-16 md:p-8 lg:p-10">
-    <div class="grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-5">
-        <div>
-            <h3 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Company</h3>
-            <ul class="text-gray-500 dark:text-gray-400">
-                <li class="mb-4">
-                    <a href="#" class=" hover:underline">About</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Careers</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Brand Center</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Blog</a>
-                </li>
-            </ul>
-        </div>
-        <div>
-            <h3 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Help center</h3>
-            <ul class="text-gray-500 dark:text-gray-400">
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Discord Server</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Twitter</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Facebook
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Contact Us</a>
-                </li>
-            </ul>
-        </div>
-        <div>
-            <h3 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Legal</h3>
-            <ul class="text-gray-500 dark:text-gray-400">
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Privacy Policy</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Licensing</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Terms</a>
-                </li>
-            </ul>
-        </div>
-        <div>
-            <h3 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Company</h3>
-            <ul class="text-gray-500 dark:text-gray-400">
-                <li class="mb-4">
-                    <a href="#" class=" hover:underline">About</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Careers</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Brand Center</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Blog</a>
-                </li>
-            </ul>
-        </div>
-        <div>
-            <h3 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Download</h3>
-            <ul class="text-gray-500 dark:text-gray-400">
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">iOS</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Android</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Windows</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">MacOS</a>
-                </li>
-            </ul>
-        </div>
-    </div>
     <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
     <div class="text-center">
         <a href="#"
@@ -101,6 +17,13 @@
                         class="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
                         {{ svg($sosial->tipe_media_sosial->icon, 'w-6 h-6') }}
                     </a>
+                </li>
+            @endforeach
+        </ul>
+        <ul class="flex justify-center mt-5 space-x-5 text-gray-500 dark:text-gray-400">
+            @foreach (App\Models\Tautan::all() as $tautan)
+                <li class="mb-4">
+                    <a href="{{ $tautan->link }}" class=" hover:underline">{{ $tautan->name }}</a>
                 </li>
             @endforeach
         </ul>
